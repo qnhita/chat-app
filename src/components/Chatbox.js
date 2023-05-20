9b946a9e679c12b7291a3bbcd1afaa3efa2f0391
qnhita <qnhita@gmail.com>
2023-05-20 20:21:10 +0300

    update repository

--- src/components/Chatbox.js
+++ src/components/Chatbox.js
@@ -1,6 +1,34 @@
 import React from 'react';
 import firebase from '../firebase';
+import { UserAuth } from '../context/AuthContext'
 
+function ChatboxContent({ chats }) {
+    const currentUser = UserAuth().currentUser;
+  
+    return (
+      <div className="chatbox pb-44 pt-20 containerWrap">
+        <ul className="chat-list px-16">
+          {chats.sort((a, b) => a.date - b.date).map(chat => {
+            const postDate = new Date(chat.date);
+  
+            return (
+              <li key={chat.id} className={`chat ${chat.user === currentUser.displayName ? "chat-start" : "chat-end"
+              }`}>
+                <div className="chat-header inline">
+                  {chat.user}
+                  <time className="text-xs opacity-50 inline">
+                    {-postDate.getDate()}/{postDate.getMonth() + 1}
+                  </time>
+                </div>
+                <div className="chat-bubble inline">{chat.message}</div>
+              </li>
+            );
+            
+          })}
+        </ul>
+      </div>
+    );
+  }
  
 class Chatbox extends React.Component {
    constructor(props) {
@@ -17,22 +45,23 @@ class Chatbox extends React.Component {
         for (let chat in getChats) {
             if(getChats[chat].message !== ''){
                 ascChats.push({
-                    id:chat,
+                    id: chat,
                     message: getChats[chat].message,
                     user: getChats[chat].user,
                     date: getChats[chat].timestamp
                 });
             }
         }
+        
+        
     const chats = ascChats.reverse();
     this.setState({chats});    
     });
    }
-   
+   render() {
+    return <ChatboxContent chats={this.state.chats} />;
+  }
 }
-
-
-
-        
+  
   
 export default Chatbox;
---
+++ src/context/AuthContext.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect, useContext ,createContext} from 'react';
+import { auth } from '../firebase';
+
+
+export const AuthContext = createContext();
+
+export function useAuth() {
+  return useContext(AuthContext);
+}
+
+export function UserAuth() {
+  const currentUser = auth.currentUser;
+  return { currentUser };
+}
+export function AuthProvider({ children }) {
+  const [currentUser, setCurrentUser] = useState();
+  const [loading, setLoading] = useState(true);
+
+  function signup(email, password) {
+    return auth.createUserWithEmailAndPassword(email, password);
+  }
+
+  function login(email, password) {
+    return auth.signInWithEmailAndPassword(email, password);
+  }
+
+  function logout() {
+    return auth.signOut();
+  }
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(user => {
+      setCurrentUser(user);
+      setLoading(false);
+    });
+
+    return unsubscribe;
+  }, []);
+
+  const value = {
+    currentUser,
+    signup,
+    login,
+    logout
+  };
+
+  return (
+    <AuthContext.Provider value={value}>
+      {!loading && children}
+    </AuthContext.Provider>
+  );
+}
