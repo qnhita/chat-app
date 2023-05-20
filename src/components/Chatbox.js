1c2f661b4b7e90bf8328ab4975738848e459277c
qnhita <qnhita@gmail.com>
2023-05-20 10:48:57 +0300

    Update repository

--- src/components/Chatbox.js
+++ src/components/Chatbox.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import firebase from '../firebase';
-import { render } from '@testing-library/react';
 
+ 
 class Chatbox extends React.Component {
    constructor(props) {
     super(props);
@@ -28,24 +28,7 @@ class Chatbox extends React.Component {
     this.setState({chats});    
     });
    }
-   render() {
-    return (
-      <div className="chatbox">
-              <ul className="chat-list bg-gray-200 p-4 rounded-lg">
-                  {this.state.chats.map((chat) =>{
-                      const postDate = new Date(chat.date);
-                      return(
-                          <li key={chat.id} className="flex mb-2">
-                              <em className="mr-2 text-gray-600">{postDate.detDate() + '/' + (postDate.getMonth() +1)}</em>
-                              <strong className="mr-2 text-blue-500">{chat.user}:</strong>
-                              <span className="text-gray-800">{chat.message}</span>
-                          </li>
-                      )
-                  })}
-              </ul>
-          </div>
-    ) 
-    }
+   
 }
 
 
